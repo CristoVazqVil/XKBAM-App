34872f90c583f7c26fb1ce128292590e7eef5900
Jasiel-Z <zS21013907@estudiantes.uv.mx>
2024-06-19 00:54:42 -0600

    Actualización: Activity para cu de empleado

---
+++ app/src/main/java/com/example/xkbam/DetallesArticuloActivity.java
@@ -0,0 +1,14 @@
+package com.example.xkbam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetallesArticuloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_articulo);
+    }
+}
