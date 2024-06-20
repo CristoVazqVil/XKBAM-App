a697d9ddf56ab2ae690196892a271e4c3e3f8edd
Dylan00Platas <131178283+Dylan00Platas@users.noreply.github.com>
2024-06-20 14:50:04 -0600

    Update MenuCuentaActivity.java

--- app/src/main/java/com/example/xkbam/main/MenuCuentaActivity.java
+++ app/src/main/java/com/example/xkbam/main/MenuCuentaActivity.java
@@ -16,7 +16,7 @@ public class MenuCuentaActivity extends AppCompatActivity {
         TextView addressTitle = findViewById(R.id.address_title);
         TextView accountTitle = findViewById(R.id.account_title);
         TextView infoTitle = findViewById(R.id.info_title);
-
+        TextView logOutTitle = findViewById(R.id.log_out_title);  // Nuevo TextView para cerrar sesión
 
         addressTitle.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,7 +26,6 @@ public class MenuCuentaActivity extends AppCompatActivity {
             }
         });
 
-
         accountTitle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,16 +34,24 @@ public class MenuCuentaActivity extends AppCompatActivity {
             }
         });
 
-
         infoTitle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MenuCuentaActivity.this, DetallesUsuarioActivity.class);
                 intent.putExtra("NUMBER", 2); // o 2, según sea necesario
                 startActivity(intent);
-
             }
         });
+
+        logOutTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cerrarSesion();
+            }
+        });
+    }
+
+    private void cerrarSesion() {
 
     }
 }
