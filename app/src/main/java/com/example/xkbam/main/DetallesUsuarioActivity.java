dfcae201c1728a8ec2e2837e303f28e7efc5ae02
Dylan00Platas <131178283+Dylan00Platas@users.noreply.github.com>
2024-06-20 15:49:41 -0600

    validaciones

--- app/src/main/java/com/example/xkbam/main/DetallesUsuarioActivity.java
+++ app/src/main/java/com/example/xkbam/main/DetallesUsuarioActivity.java
@@ -12,19 +12,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.xkbam.api.ApiConexion;
 import com.example.xkbam.databinding.ActivityDetallesUsuarioBinding;
 import com.example.xkbam.utilidades.SesionSingleton;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class DetallesUsuarioActivity extends AppCompatActivity {
 
     private ActivityDetallesUsuarioBinding binding;
-
-    // Declare EditText variables as member variables
     private EditText usernameEditText;
     private EditText firstNameEditText;
     private EditText paternoNameEditText;
@@ -37,11 +35,9 @@ public class DetallesUsuarioActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityDetallesUsuarioBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Initialize the EditText variables
         usernameEditText = findViewById(R.id.username);
         firstNameEditText = findViewById(R.id.first_name);
         paternoNameEditText = findViewById(R.id.paterno_name);
@@ -54,11 +50,8 @@ public class DetallesUsuarioActivity extends AppCompatActivity {
         Button saveButton = findViewById(R.id.save_button);
         Button modifyButton = findViewById(R.id.modify_button);
 
-
-        // Obtain the intent and the number
         int number = getIntent().getIntExtra("NUMBER", 0);
 
-        // Check the number and adjust visibility and actions accordingly
         if (number == 1) {
             saveButton.setVisibility(View.VISIBLE);
         } else if (number == 2) {
@@ -73,7 +66,8 @@ public class DetallesUsuarioActivity extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Obtener los valores de los campos de entrada
+                if (!validarCampos() || !validarUsuario() || !validarEmail() || !validarContraseña()) return;
+
                 String username = usernameEditText.getText().toString();
                 String firstName = firstNameEditText.getText().toString();
                 String paternoName = paternoNameEditText.getText().toString();
@@ -81,17 +75,14 @@ public class DetallesUsuarioActivity extends AppCompatActivity {
                 String gender = genderSpinner.getSelectedItem().toString();
                 String email = emailEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
+                int genderValue = gender.equals("Masculino") ? 1 : 2;
 
-                // Convertir género a entero
-                int genderValue = gender.equals("Masculino") ? 1 : 2; // Ajusta según tus valores de género
-
-                // Crear JSON
                 JSONObject usuarioJson = new JSONObject();
                 JSONObject cuentaJson = new JSONObject();
                 JSONObject dataJson = new JSONObject();
 
                 try {
-                    usuarioJson.put("usuario", username); // Ajusta según corresponda
+                    usuarioJson.put("usuario", username);
                     usuarioJson.put("nombre", firstName);
                     usuarioJson.put("apellidoPaterno", paternoName);
                     usuarioJson.put("apellidoMaterno", maternoName);
@@ -99,15 +90,11 @@ public class DetallesUsuarioActivity extends AppCompatActivity {
 
                     cuentaJson.put("correo", email);
                     cuentaJson.put("contrasena", password);
-                    cuentaJson.put("idRol", 2); // Ajusta según corresponda
+                    cuentaJson.put("idRol", 2);
 
                     dataJson.put("usuario", usuarioJson);
                     dataJson.put("cuenta", cuentaJson);
 
-
-
-
-                    // Enviar solicitud a la API
                     ApiConexion.enviarRequestAsincrono("POST", "usuarios", dataJson.toString(), false, new Callback() {
                         @Override
                         public void onFailure(Call call, IOException e) {
@@ -127,7 +114,6 @@ public class DetallesUsuarioActivity extends AppCompatActivity {
                                     public void run() {
                                         Toast.makeText(DetallesUsuarioActivity.this, "Datos guardados correctamente", Toast.LENGTH_SHORT).show();
                                         finish();
-
                                     }
                                 });
                             } else {
@@ -148,42 +134,36 @@ public class DetallesUsuarioActivity extends AppCompatActivity {
             }
         });
 
-
         modifyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Obtener los valores de los campos de entrada
+                if (!validarCampos() || !validarEmail()) return;
+
                 String username = usernameEditText.getText().toString();
                 String firstName = firstNameEditText.getText().toString();
                 String paternoName = paternoNameEditText.getText().toString();
                 String maternoName = maternoNameEditText.getText().toString();
                 String gender = genderSpinner.getSelectedItem().toString();
                 String email = emailEditText.getText().toString();
+                int genderValue = gender.equals("Masculino") ? 1 : 2;
 
-                // Convertir género a entero
-                int genderValue = gender.equals("Masculino") ? 1 : 2; // Ajusta según tus valores de género
-
-                // Crear JSON
                 JSONObject usuarioJson = new JSONObject();
                 JSONObject cuentaJson = new JSONObject();
                 JSONObject dataJson = new JSONObject();
 
                 try {
-                    usuarioJson.put("usuario", username); // Ajusta según corresponda
+                    usuarioJson.put("usuario", username);
                     usuarioJson.put("nombre", firstName);
                     usuarioJson.put("apellidoPaterno", paternoName);
                     usuarioJson.put("apellidoMaterno", maternoName);
                     usuarioJson.put("genero", genderValue);
 
                     cuentaJson.put("correo", email);
-                    cuentaJson.put("idRol", 2); // Ajusta según corresponda
+                    cuentaJson.put("idRol", 2);
 
                     dataJson.put("usuario", usuarioJson);
                     dataJson.put("cuenta", cuentaJson);
 
-
-
-                    // Enviar solicitud a la API
                     ApiConexion.enviarRequestAsincrono("PUT", "usuarios/" + username, dataJson.toString(), true, new Callback() {
                         @Override
                         public void onFailure(Call call, IOException e) {
@@ -222,8 +202,43 @@ public class DetallesUsuarioActivity extends AppCompatActivity {
                 }
             }
         });
+    }
 
+    private boolean validarCampos() {
+        if (usernameEditText.getText().toString().isEmpty() || firstNameEditText.getText().toString().isEmpty() || paternoNameEditText.getText().toString().isEmpty() || maternoNameEditText.getText().toString().isEmpty() || emailEditText.getText().toString().isEmpty()) {
+            mostrarDialogo("Campos incompletos", "Todos los campos son obligatorios");
+            return false;
+        }
+        return true;
+    }
 
+    private boolean validarUsuario() {
+        String username = usernameEditText.getText().toString();
+        if (username.length() < 6 || username.length() > 30) {
+            mostrarDialogo("Usuario inválido", "El usuario debe tener entre 6 y 30 caracteres");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validarEmail() {
+        String email = emailEditText.getText().toString();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        if (!Pattern.matches(emailPattern, email)) {
+            mostrarDialogo("Correo inválido", "Por favor, ingrese un correo electrónico válido");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validarContraseña() {
+        String password = passwordEditText.getText().toString();
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+        if (!Pattern.matches(regex, password)) {
+            mostrarDialogo("Contraseña inválida", "La contraseña debe tener al menos 8 caracteres, incluyendo una mayúscula, una minúscula y un carácter especial");
+            return false;
+        }
+        return true;
     }
 
     private void obtenerDatosUsuario(String usuario) {
@@ -257,7 +272,7 @@ public class DetallesUsuarioActivity extends AppCompatActivity {
                                     maternoNameEditText.setText(usuarioObj.getString("apellidoMaterno"));
                                     genderSpinner.setSelection(usuarioObj.getInt("genero") == 1 ? 0 : 1);
                                     emailEditText.setText(cuentaObj.getString("correo"));
-                                    passwordEditText.setText(""); // No se recomienda mostrar la contraseña real
+                                    passwordEditText.setText("");
                                 } catch (JSONException e) {
                                     e.printStackTrace();
                                     mostrarDialogo("Error al procesar datos", e.getMessage());
