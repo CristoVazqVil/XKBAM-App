b0472da0fc245d4a248089015552805dd368f643
Dylan00Platas <131178283+Dylan00Platas@users.noreply.github.com>
2024-06-21 13:35:37 -0600

    menu cliente

--- app/src/main/java/com/example/xkbam/main/MenuCuentaActivity.java
+++ app/src/main/java/com/example/xkbam/main/MenuCuentaActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.xkbam.utilidades.SesionSingleton;
 
@@ -18,8 +19,22 @@ public class MenuCuentaActivity extends AppCompatActivity {
         TextView addressTitle = findViewById(R.id.address_title);
         TextView accountTitle = findViewById(R.id.account_title);
         TextView infoTitle = findViewById(R.id.info_title);
-        TextView logOutTitle = findViewById(R.id.log_out_title);  // Nuevo TextView para cerrar sesión
+        TextView logOutTitle = findViewById(R.id.log_out_title);
+        TextView addArticleTitle = findViewById(R.id.add_article_title);
+        TextView salesReportTitle = findViewById(R.id.sales_report_title);
 
+        int idRol = SesionSingleton.getInstance().getRol();
+
+        if (idRol == 1) {
+            addArticleTitle.setVisibility(View.VISIBLE);
+            salesReportTitle.setVisibility(View.VISIBLE);
+            addressTitle.setVisibility(View.GONE);
+            accountTitle.setVisibility(View.GONE);
+            infoTitle.setVisibility(View.GONE);
+        } else {
+            addArticleTitle.setVisibility(View.GONE);
+            salesReportTitle.setVisibility(View.GONE);
+        }
 
         addressTitle.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,7 +56,7 @@ public class MenuCuentaActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MenuCuentaActivity.this, DetallesUsuarioActivity.class);
-                intent.putExtra("NUMBER", 2); // o 2, según sea necesario
+                intent.putExtra("NUMBER", 2);
                 startActivity(intent);
             }
         });
@@ -52,9 +67,30 @@ public class MenuCuentaActivity extends AppCompatActivity {
                 cerrarSesion();
             }
         });
+
+        addArticleTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MenuCuentaActivity.this, "Añadir Articulo fue presionado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        salesReportTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MenuCuentaActivity.this, "Reporte de Ventas fue presionado", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void cerrarSesion() {
+<<<<<<< Updated upstream
 
+=======
+        Intent intent = new Intent(MenuCuentaActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        MenuCuentaActivity.this.finish();
+>>>>>>> Stashed changes
     }
 }
